db1ca20c7549eca0c16861c3540a5e3ce6116586
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-23 17:35:44 +0900

    クラスにトライしよう　ステップ4　課題②

--- src/public/Q2.php
+++ src/public/Q2.php
@@ -1,6 +1,21 @@
 <?php
-for ($i = 1; $i <= 10; $i++) {
-    if ($i % 2 == 0) {
-			echo $i . "<br>";
-		}
+class multipleOfTwo {
+	public function printEvenNumbers($start, $end) {
+			for ($i = $start; $i <= $end; $i++) {
+					if ($i % 2 == 0) {
+							echo $i . "<br>";
+					}
+			}
+	}
 }
+
+$object = new multipleOfTwo();
+$object->printEvenNumbers(1, 10);
+
+
+
+// for ($i = 1; $i <= 10; $i++) {
+//     if ($i % 2 == 0) {
+// 			echo $i . "<br>";
+// 		}
+// }
