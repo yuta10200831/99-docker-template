8dbc51834913d0f6ef7eaa6ce274cec7bff2d7f7
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-23 17:48:29 +0900

    クラスにトライしよう　ステップ4　課題③　提出

--- src/public/Q3.php
+++ src/public/Q3.php
@@ -1,13 +1,31 @@
 <?php
-for ($i = 1; $i < 10; $i++) {
-  if ($i % 2 == 0 && $i % 3 == 0) {
-      echo $i . "は2の倍数かつ3の倍数です" . "<br>";
-  } elseif ($i % 2 == 0) {
-      echo $i . "は2の倍数です" . "<br>";
-  } elseif ($i % 3 == 0) {
-      echo $i . "は3の倍数です" . "<br>";
-  }
+class NumberClassFilter {
+    public function classNumbers($start, $end) {
+        for ($i = $start; $i < $end; $i++) {
+            if ($i % 2 == 0 && $i % 3 == 0) {
+                echo $i . "は2の倍数かつ3の倍数です" . "<br>";
+            } elseif ($i % 2 == 0) {
+                echo $i . "は2の倍数です" . "<br>";
+            } elseif ($i % 3 == 0) {
+                echo $i . "は3の倍数です" . "<br>";
+            }
+        }
+    }
 }
+
+$object = new NumberClassFilter();
+$object->classNumbers(1, 10);
+
+
+// for ($i = 1; $i < 10; $i++) {
+//   if ($i % 2 == 0 && $i % 3 == 0) {
+//       echo $i . "は2の倍数かつ3の倍数です" . "<br>";
+//   } elseif ($i % 2 == 0) {
+//       echo $i . "は2の倍数です" . "<br>";
+//   } elseif ($i % 3 == 0) {
+//       echo $i . "は3の倍数です" . "<br>";
+//   }
+// }
 
 // 2は2の倍数です
 // 3は3の倍数です
