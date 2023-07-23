f86f092aa28ad2d31eb19deb18a87260bc07be42
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-23 16:44:09 +0900

    クラスにトライしよう　ステップ4　課題①　提出

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,20 @@
 <?php
-for ($i = 1; $i <= 6; $i++) {
-    if ($i % 3 == 0) {
-			echo $i . "<br>";
-		}
+class MyClass {
+	public function multipleOfThree() {
+			for ($i = 1; $i <= 6; $i++) {
+					if ($i % 3 == 0) {
+							echo $i . "<br>";
+					}
+			}
+	}
 }
+
+$object = new MyClass();
+$object->multipleOfThree();
+
+
+// for ($i = 1; $i <= 6; $i++) {
+//     if ($i % 3 == 0) {
+// 			echo $i . "<br>";
+// 		}
+// }
