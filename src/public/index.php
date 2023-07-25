16a77ddc46c45ca0f8695ec99071bb1f629f6b19
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-25 09:31:34 +0900

    クラスの型にトライしよう　ステップ4　課題①　提出

--- src/public/index.php
+++ src/public/index.php
@@ -1,17 +1,20 @@
 <?php
 class MyClass {
-	public function multipleOfThree() {
-			for ($i = 1; $i <= 6; $i++) {
+	public function multipleOfThree(int $limit) {
+			for ($i = 1; $i <= $limit; $i++) {
 					if ($i % 3 == 0) {
 							echo $i . "<br>";
 					}
 			}
-	}
-}
+	}}
 
 $object = new MyClass();
-$object->multipleOfThree();
+$limit = 6;
+$object->multipleOfThree($limit);
 
+// (期待する出力)
+// 3
+// 6
 
 // for ($i = 1; $i <= 6; $i++) {
 //     if ($i % 3 == 0) {
